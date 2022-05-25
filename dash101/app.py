0b295ae39c20cd0ca1302865edf651ee9b1e2f10
clumdee <chatdanai.l@gmail.com>
2022-05-25 15:45:52 +0700

    read graph interactions

--- dash101/app.py
+++ dash101/app.py
@@ -1,27 +1,114 @@
-from dash import Dash, dcc, html, Input, Output, callback
+import json
 
-app = Dash(__name__)
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
 
-my_input = dcc.Input(value='initial value', type='text')
-my_output = html.Div()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+df = pd.DataFrame({
+    "x": [1,2,1,2],
+    "y": [1,2,3,4],
+    "customdata": [1,2,3,4],
+    "fruit": ["apple", "apple", "orange", "orange"]
+})
+
+fig = px.scatter(df, x="x", y="y", color="fruit", custom_data=["customdata"])
+
+fig.update_layout(clickmode='event+select')
+
+fig.update_traces(marker_size=20)
 
 app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "Input: ",
-        my_input
-    ]),
-    html.Br(),
-    my_output
+    dcc.Graph(
+        id='basic-interactions',
+        figure=fig
+    ),
+
+    html.Div(className='row', children=[
+        html.Div([
+            dcc.Markdown("""
+                **Hover Data**
+
+                Mouse over values in the graph.
+            """),
+            html.Pre(id='hover-data', style=styles['pre'])
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Click Data**
+
+                Click on points in the graph.
+            """),
+            html.Pre(id='click-data', style=styles['pre']),
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Selection Data**
+
+                Choose the lasso or rectangle tool in the graph's menu
+                bar and then select points in the graph.
+
+                Note that if `layout.clickmode = 'event+select'`, selection data also
+                accumulates (or un-accumulates) selected data if you hold down the shift
+                button while clicking.
+            """),
+            html.Pre(id='selected-data', style=styles['pre']),
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown("""
+                **Zoom and Relayout Data**
+
+                Click and drag on the graph to zoom or click on the zoom
+                buttons in the graph's menu bar.
+                Clicking on legend items will also fire
+                this event.
+            """),
+            html.Pre(id='relayout-data', style=styles['pre']),
+        ], className='three columns')
+    ])
 ])
 
 
-@callback(
-    Output(my_output, component_property='children'),
-    Input(my_input, component_property='value')
-)
-def update_output_div(input_value):
-    return f'Output: {input_value}'
+@app.callback(
+    Output('hover-data', 'children'),
+    Input('basic-interactions', 'hoverData'))
+def display_hover_data(hoverData):
+    return json.dumps(hoverData, indent=2)
+
+
+@app.callback(
+    Output('click-data', 'children'),
+    Input('basic-interactions', 'clickData'))
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+
+
+@app.callback(
+    Output('selected-data', 'children'),
+    Input('basic-interactions', 'selectedData'))
+def display_selected_data(selectedData):
+    return json.dumps(selectedData, indent=2)
+
+
+@app.callback(
+    Output('relayout-data', 'children'),
+    Input('basic-interactions', 'relayoutData'))
+def display_relayout_data(relayoutData):
+    return json.dumps(relayoutData, indent=2)
 
 
 if __name__ == '__main__':
